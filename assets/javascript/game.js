2120df69880d5945afc64e5190d00835896aa000
DocHolidayDeluxe <dabauer@me.com>
2019-02-18 22:33:24 -0500

    css added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,17 @@
-// create random alphabet letter
-
-
-// create user input
-
-// var for wins, losses, no of guesses
-
 
 var wins = 0
 var losses = 0
 var guessesLeft = 10
 var guesses = 0
 
-
 var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
 var guessesLeftText = document.getElementById("guessesLeft");
 var guessesText = document.getElementById("guesses");
 
-
 var computerChoices = "abcdefghijklmnopqrstuvwxyz".split("");
 
-// for (var i = 1; i >= guessesLeft; ) {
-
-
 document.onkeyup = function (event) {
-
 
     var userGuess = event.key;
 
@@ -37,8 +24,6 @@ document.onkeyup = function (event) {
         else { losses++; guessesLeft--; guesses++; }
     }
     else {wins = 0; losses = 0; guessesLeft = 10; guesses = 0 }
-
-
 
     winsText.textContent = wins;
     lossesText.textContent = losses;
